7de0afad554070d0b16b35ebe99a3c3c15c55ab6
Andrew Reid <andrew.ce.reid@gmail.com>
2017-12-05 21:01:53 -0500

    Day 5. Run-times of naive Haskell getting a bit long.

---
+++ day5a.hs
@@ -0,0 +1,26 @@
+import Data.List
+import Data.Char
+import Data.String
+import qualified Data.Map as Map
+
+
+
+-- Apparently the Maybe/Just is required by Map.update semantics.
+iinc :: Int -> Maybe Int
+iinc x = Just (x+1)
+
+
+-- index, step count, map, counts to halt.
+haltstep :: Int -> Int -> Map.Map Int Int -> Int
+haltstep x c m = do
+  let nx = x + case (Map.lookup x m) of Just v -> v
+                                        _ -> 0 
+  let m2 = Map.update iinc x m 
+  if (Map.member nx m2) then (haltstep nx (c+1) m2) else (c+1)
+
+main = do 
+  fdat <- readFile "day5.txt"
+  let lns = lines fdat
+  let m = Map.fromList ( zip (iterate (\x -> x+1) 0) 
+                             (map (\x -> read x :: Int) lns) )
+  putStrLn ( show ( haltstep 0 0 m ))
